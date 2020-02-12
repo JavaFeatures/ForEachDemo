56dac5f2344c5b848ac14efdc74c1b0f47650c49
iscipar <iscipar@hotmail.com>
2020-02-12 16:48:38 +0100

    Se crea el proyecto

---
+++ src/demo/ForEachDemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Cat", "Dog", "Mouse");
+		list.forEach(str -> System.out.println("Animal is: " + str));
+//		for (String str:list) {
+//			System.out.println("Animal is: " + str);
+//		}
+	}
+
+}
